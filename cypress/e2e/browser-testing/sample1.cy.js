adf8f112074309c01ea04e17bd5720af7ac5a670
Ana Santos <firmino.anac@gmail.com>
2023-04-04 19:55:32 -0300

    browser bad pratice

--- cypress/e2e/browser-testing/sample1.cy.js
+++ cypress/e2e/browser-testing/sample1.cy.js
@@ -4,8 +4,9 @@ describe('Browser testing bad practice - anchor href', () => {
   })
 
   it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
+    cy.contains('.nav a', 'Login')
+    .should('have.attr', 'href', '/login')
+    .and('not.have.attr', 'target')
 
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
   })
 })
